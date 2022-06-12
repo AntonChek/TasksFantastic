47a31b61ef2abb10eea4145c057b8198a2cf5c6c
AntonChek <07anton99@mail.ru>
2022-06-12 14:33:36 +0900

    First commit

---
+++ Task1/ConverterForFantastic/ConverterForFantastic/ConverterForFantastic.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <windows.h>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "");
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	string str1;
+
+	cout << "Введите строку: ";
+	getline(cin, str1);
+	transform(str1.begin(), str1.end(), str1.begin(), tolower);
+	cout << "Конвертированная строка: ";
+
+	for (char ch1 : str1)
+	{
+		size_t count = 0;
+		for (char ch2 : str1)
+		{
+			if (ch1 == ch2)
+			{
+				count++;
+			}
+		}
+
+		if (count > 1)
+		{
+			cout << ")"; 
+		}
+		else
+		{
+			cout << "(";
+		}
+	}
+
+	cin.get();
+	return 0;
+}
